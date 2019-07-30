df3eb2831daa0dfb1506bff4ba7c8f19908011fb
SiwenC106 <Siwen.Cui@outlook.com>
2019-07-30 13:44:25 -0700

    DNS
    Configure DNS

---
+++ DNS.ps1
@@ -0,0 +1,22 @@
+﻿**Create Windows-Feature**
+
+    Install-WindowsFeature -Name DNS -IncludeManagementTool
+
+**DNS**
+        
+
+    Display-DNS Cache
+
+        Server side
+            Show-DNSServerCache
+            Clear-DnsServerCache
+        Client side
+            ipconfig /displayDNS
+            ipconfig /flushdns
+
+    Create Forwarders
+            Regular Forwarder
+                Add-DnsServerForwarder 8.8.8.8 
+            Conditional Forwarder 
+                Add-DnsServerConditionalForwarderZone google.com 8.8.4.4
+                 
